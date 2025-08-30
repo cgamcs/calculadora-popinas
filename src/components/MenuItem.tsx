0c05e9e33b2deae095eb762b36e5519d60d74ddf
César García <cesar.garciamcs@outlook.com>
2025-08-30 17:11:49 -0600

    creando funcion para añadir elemento a la orden

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,13 +2,15 @@ import type { MenuItems } from "../types"
 
 type MenuItemsProps = {
     item: MenuItems
+    addItem: () => void
 }
 
-function MenuItem({ item }: MenuItemsProps) {
+function MenuItem({ item, addItem }: MenuItemsProps) {
     const { name, price } = item
     return (
         <button
             className="border border-teal-400 hover:bg-teal-200 w-full p-3 rounded-md flex justify-between cursor-pointer"
+            onClick={() => addItem()}
         >
             <p>{name}</p>
             <p className="font-black">${price}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,8 +4,11 @@ import type { OrderItem } from "../types"
 function useOrder() {
   const [order, useOrder] = useState<OrderItem[]>([])
 
+  const addItem = () => {
+    console.log('Agregando...')
+  }
   return {
-
+    addItem
   }
 }
 
